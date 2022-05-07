ea071c2ddd46f95a52eeb0c36657ed9ccb53d42c
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-07 16:08:09 -0700

    added folders for components

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+        <nav id="nav" class="navbar">
+            <h2 class="nav-btn" id="about-me-btn">About Me</h2>
+            <h2 class="nav-btn" id="portfolio-btn">My Portfolio</h2>
+            <h2 class="nav-btn" id="contact-me-btn">Contact Me</h2>
+        </nav>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/Page/index.js
@@ -0,0 +1,17 @@
+//import: about, portfolio, contactme
+// function Page({ currentPage }) {
+//     const renderPage = () => {
+//         switch (currentPage.name) {
+//             case 'about me':
+//                 return <About />
+//             case 'portfolio':
+//                 return <Portfolio />;
+//             case 'contact':
+//                 return <Contact />;
+//             case 'greeting':
+//                 return <Greeting />;
+//             default:
+//                 return <Greeting />
+//         };
+//     };
+// };
