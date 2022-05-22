25c7b8fb101e7c9e85190eed6e99a17a680ad836
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-22 16:23:01 -0700

    inital styling of welcome and about me pages

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -7,7 +7,7 @@ function AboutMe() {
         <>
         <section className="bg">
             <div className="content">
-                <h3 className="m-5">Let me introduce myself!</h3>
+                <h1>Let me introduce myself!</h1>
                 <p>I'm a programmer based in Seattle, WA. I create responsive websites that look and function great across all platforms. I'm currently working as an IT Recruiter, and through this role fell in love with coding and all things tech. Now I'm looking to apply my skills full-time as a Web Developer!</p>
                 <p>When I'm not busy coding, I like to keep busy with reading, running, yoga, meditation, exploring the amazing sights of the Pacific Northwest with my husband, and playing with our eccentric cat.</p>
             </div>
