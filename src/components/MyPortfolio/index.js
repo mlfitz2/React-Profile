393aa05b6cd907a1219f601a3443d36820767d63
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-22 16:56:22 -0700

    basic formatting of portfolio page

--- src/components/MyPortfolio/index.js
+++ src/components/MyPortfolio/index.js
@@ -3,6 +3,7 @@ import '../Page/style.css';
 import './style.css';
 import project1 from './pubcrawler.JPG'
 import project2 from './happy-place.JPG'
+import project3 from './ethyl.JPG'
 
 function MyPortfolio(props) {
     const {pages=[], setCurrentPage}=props;
@@ -10,21 +11,21 @@ function MyPortfolio(props) {
         <>
         <section className="bg">
             <div className="content">
-                <h3>Here are some cool projects I've made!</h3>
-                <p>Click on a project for more info.</p>
+                <h1>Here are some cool projects I've made!</h1>
+                <h2>Click on a project for more info.</h2>
                 
-                <div>
-                    <div>
-                        <h4>PubCrawler</h4>
-                        <span onClick={()=>setCurrentPage(pages[4])}><img src={project1} alt="pubcrawler-pic"/></span>
+                <div className="projects-area">
+                    <div className="project-card">
+                        <h3>PubCrawler</h3>
+                        <img src={project1} alt="pubcrawler-pic"/>
                     </div>
-                    <div>
-                        <h4>The Happy Place</h4>
+                    <div className="project-card">
+                        <h3>The Happy Place</h3>
                         <img alt="happy-place-pic" src={project2}/>
                     </div>
-                    <div>
-                        <h4>Note Taker</h4>
-                        <img id="note-taker-pic" className="portfolio-pic" alt="pic" src="assets/note-taker.JPG"/>
+                    <div className="project-card">
+                        <h3>Ethyl</h3>
+                        <img alt="ethyl-pic" src={project3}/>
                     </div>
                 </div>
             </div>
