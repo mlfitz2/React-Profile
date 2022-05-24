277f0b5c5f2108cf0c686c42c857939edab72e81
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-23 19:41:23 -0700

    started styling for projects 1-3

--- src/components/Project1/index.js
+++ src/components/Project1/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import '../Page/style.css';
 import './style.css';
+import pubcrawler from './pubcrawler.JPG'
 
 function Project1() {
     return (
         <>
             <section className="bg">
                 <div className="content">
-                    <h3>PubCrawler</h3>
-                    <p>PubCrawler allows users to select a city, or use their current location, to see a map of breweries in the area. Select the ones you like, and the app will display the most efficient route for a pub crawl between them. You can save your favorite routes to local storage and refer back to them next time.</p>
-                    <div>
-                        <img className="project-demo-pic" alt="project-pic" src="assets/pubcrawler.JPG"/>
-                        <div>
-                            <a href="https://benfok.github.io/pub-crawler" target="_blank" rel="noreferrer">Deployed App</a>
-                            <a href="https://github.com/mlfitz2/pub-crawler" target="_blank" rel="noreferrer">GitHub</a>
+                    <h3 className="project-h1">PubCrawler</h3>
+                    <p className="project-text">PubCrawler allows users to select a city, or use their current location, to see a map of breweries in the area. Select the ones you like, and the app will display the most efficient route for a pub crawl between them. You can save your favorite routes to local storage and refer back to them next time.</p>
+                    <div className="project-display">
+                        <img className="project-pic" alt="project-pic" src={pubcrawler}/>
+                        <div className="project-links-area">
+                            <a className="project-link" href="https://benfok.github.io/pub-crawler" target="_blank" rel="noreferrer">Deployed App</a>
+                            <a className="project-link" href="https://github.com/mlfitz2/pub-crawler" target="_blank" rel="noreferrer">GitHub</a>
                         </div>
                     </div>
                 </div>
