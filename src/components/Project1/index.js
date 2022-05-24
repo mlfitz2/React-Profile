93f70100e470d253ef50972417d2644a35ba046f
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-23 20:07:29 -0700

    formatted projects 2 & 3

--- src/components/Project1/index.js
+++ src/components/Project1/index.js
@@ -8,7 +8,7 @@ function Project1() {
         <>
             <section className="bg">
                 <div className="content">
-                    <h3 className="project-h1">PubCrawler</h3>
+                    <h1 className="project-h1">PubCrawler</h1>
                     <p className="project-text">PubCrawler allows users to select a city, or use their current location, to see a map of breweries in the area. Select the ones you like, and the app will display the most efficient route for a pub crawl between them. You can save your favorite routes to local storage and refer back to them next time.</p>
                     <div className="project-display">
                         <img className="project-pic" alt="project-pic" src={pubcrawler}/>
--- src/components/Project2/index.js
+++ src/components/Project2/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import '../Page/style.css';
 import './style.css';
+import happyPlace from './happy-place.JPG'
 
 function Project2() {
     return (
         <>
             <section className="bg">
                 <div className="content">
-                    <h3>The Happy Place</h3>
-                    <p>The Happy Place is for users who are tired of the toxicity of mainstream social networks. This app is a place to reflect on what we're grateful for, and can be shared publicly or kept private like a journal. Inspiring quotes also help to brighten your day!</p>
-                    <div>
-                        <img alt="pic-of-project" src="assets/happy-place.JPG"/>
-                        <div>
-                            <a href="https://the-happy-place-app.herokuapp.com/" target="_blank" rel="noreferrer">Deployed App</a>
-                            <a href="https://github.com/mlfitz2/The-Happy-Place" target="_blank" rel="noreferrer">GitHub</a>
+                    <h1 className="project-h1">The Happy Place</h1>
+                    <p className="project-text">The Happy Place is for users who are tired of the toxicity of mainstream social networks. This app is a place to reflect on what we're grateful for, and can be shared publicly or kept private like a journal. Inspiring quotes also help to brighten your day!</p>
+                    <div className="project-display">
+                        <img className="project-pic" alt="pic-of-project" src={happyPlace}/>
+                        <div className="project-links-area">
+                            <a className="project-link" href="https://the-happy-place-app.herokuapp.com/" target="_blank" rel="noreferrer">Deployed App</a>
+                            <a className="project-link" href="https://github.com/mlfitz2/The-Happy-Place" target="_blank" rel="noreferrer">GitHub</a>
                         </div>
                     </div>
 
